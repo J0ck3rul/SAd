6a1c88fb4e7019216941821a9955cf664f4a210c
andreicalinciuc <calinciuc.andrei@yahoo.com>
2019-03-25 17:23:20 +0200

    PKG_CONTENT_MATCHES

--- backend/pmlib/aptlib.py
+++ backend/pmlib/aptlib.py
@@ -3,20 +3,44 @@ import json
 import re
 from copy import deepcopy
 import time
+PKG_CONTENT_MATCHES = {
+    r"^Description[a-zA-Z\-]*: ": "_description",
+    r"^Version: ": "_version",
+    r"^Depends: ": "_depends",
+    r"^Pre-Depends: ":"_pre-depends",
+    r"^Conflicts: ": "_conflicts",
+    r"^Breaks: ": "_breaks",
+    r"^Replaces: ": "_replaces",
+    r"^Installed-Size: ": "_installed_size",    # in KB
+    r"^Size: ": "_download_size",               #in B
+    r"^Homepage: ": "_homepage",
+    r"^Maintainer: ": "_maintainer"
+     
 
-PKG_TEMPLATE ={
-        "Name" : "",
-        "Description" :"",
-        "Version": "",
-        "PreDepends" : [],
-        "Depends": [],
-        "Conflicts": [],
-        "Breaks": [],
-        "Replaces":[],
-        "Suggests":[],
-        "Recommends": []
-    }
 
+}
+
+PKG_TEMPLATE = {
+    "Name" : "",
+    "Description" :"",
+    "Version": "",
+    "PreDepends" : [],
+    "Depends": [],
+    "Conflicts": [],
+    "Breaks": [],
+    "Replaces":[]
+}
+
+def apt_show(pkg_name):
+    cmd_output = subprocess.check_output(['apt-cache',"show",pkg_name])
+    pkg = deepcopy(PKG_TEMPLATE)
+    for line in cmd_output.split("\n"):
+        match=re.search(r"^[a-zA-Z\.\-]+",line)
+        if match:
+            line=re.sub('Description:',"",line)
+            print(line)
+
+    
 
 def apt_search(pkg_name):
     pkg_list = subprocess.check_output(['apt-cache',"search",pkg_name])
@@ -45,3 +69,5 @@ def apt_get_pkg_obj(pkg_name):
 def test():
     print("Debian i guess")
     
+if __name__ == "__main__":
+    apt_show("python")
