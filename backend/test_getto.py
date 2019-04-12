b1795f9e317eef6a0470e1d75d4a35c541306b0a
Radu Florin MARINOIU <rmarinoiu@bitdefender.com>
2019-04-12 11:11:07 +0300

    DAB ON THEM DEBS

--- backend/test_getto.py
+++ backend/test_getto.py
@@ -3,6 +3,6 @@ import getto
 # print getto.get_all_distros()
 # getto.pull_all_packages_into_db()
 
-for binary in getto.get_package_binaries("firefox", "ubuntu", "bionic", "amd64"):
-    print binary.self_link
+for source in getto.get_package_sources("firefox", "ubuntu", "bionic"):
+    print source.binaryFileUrls()
 # http://launchpadlibrarian.net/416686702/firefox_66.0.2+build1-0ubuntu0.18.04.1_amd64.deb
