6072cc0c53d4ec35d494bc7ba0079e2e1aeb81ee
Radu Florin MARINOIU <rmarinoiu@bitdefender.com>
2019-04-17 20:02:18 +0300

    Changes to test_getto

--- backend/test_getto.py
+++ backend/test_getto.py
@@ -14,7 +14,7 @@ def get_installer_package(pkg_name, distro_name, series_name, distro_arch):
                 return deb
             # if "{}_".format(source_pkg.source_package_name) in deb:
             #     print "{}\n{}_{}\n\n".format(deb,source_pkg.source_package_name,base_version)
-    
+    return None
     print "Package not found, here are some similar packages:"
     similar_package_sources = getto.find_package_source(pkg_name, distro_name)
     if similar_package_sources:
@@ -29,4 +29,18 @@ def get_installer_package(pkg_name, distro_name, series_name, distro_arch):
         return ""
 
 # print getto.find_package_source("samba", "ubuntu")
-print get_installer_package("erlang-guestfs", "ubuntu", "bionic", "amd64")
+with open("testfile.txt", "r") as f:
+    missing_count = 0
+    package_list = f.read().split('\n\n')
+    for pkg in package_list:
+        for pkg_prop in pkg.split('\n'):
+            if re.search(r"Package: ", pkg_prop):
+                pkg_name = re.sub(r"Package: ", "", pkg_prop)
+                if None is get_installer_package(pkg_name, "ubuntu", "bionic", "amd64"):
+                    print pkg_name + " not found."
+                    missing_count += 1
+    print "Missing {} packages, total {}.".format(missing_count, len(package_list))
+# print get_installer_package("erlang-guestfs", "ubuntu", "bionic", "amd64")
+
+#IMPORTANT
+#chdist for getting packages from other distros
