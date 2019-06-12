c341db4ccb02c788d7976b0683f5e7b9c43b00d1
Radu Florin MARINOIU <rmarinoiu@bitdefender.com>
2019-06-12 19:02:35 +0300

    Finished package search on launchpad

--- backend/LaunchpadService/main.py
+++ backend/LaunchpadService/main.py
@@ -1,25 +1,24 @@
-import sys, os
-print os.path.abspath('.')
-sys.path.insert(0, os.path.abspath('.'))
-from PackageClass import Package
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+from PackageClass.package import Package
 
-import functions
-import functions2
+import lplib
 
 def FindPackages(keyword):
-    found_packages_names = list(functions.find_package_source(keyword, "ubuntu"))
+    found_packages_names = list(lplib.find_package_source(keyword, "ubuntu"))
     for pkg_name in found_packages_names:
-        print "DEP:", functions.get_all_dependencies(pkg_name, "ubuntu")
+        print pkg_name
 
+lplib.find_projects("python")
 
-# FindPackages("apache2")
-functions.get_all_series()
+# print functions.get_all_series()
 
-print functions.get_distro("ubuntu").series_collection_link
-pkg_list = functions.get_package_sources("apache2", "ubuntu", "bionic")
-for pkg in pkg_list:
-        print pkg.display_name
-        print pkg.source_package_name
-        print pkg.source_package_version
-        print ""
+# print functions.get_distro("ubuntu").series_collection_link
+# pkg_list = functions.get_package_sources("apache2", "ubuntu", "bionic")
+# for pkg in pkg_list:
+#         print pkg.display_name
+#         print pkg.source_package_name
+#         print pkg.source_package_version
+#         print ""
 # pkg = functions.get_package_binaries("apache2", "ubuntu", "bionic", "amd64")[0]
