69dcca5cfecbe7a240b53eaa5ef603e90d7132da
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-06-17 22:34:43 +0300

    Started working on the dependency solver.

--- backend/UbuntuArchiveService/dependencysolver.py
+++ backend/UbuntuArchiveService/dependencysolver.py
@@ -3,15 +3,39 @@ import apt_pkg
 
 
 SYMBOL_TO_COMPARISON_RESULT_TABLE = {
-    ">= ": []
+    ">> ": [1],
+    "= ": [0],
+    "<<": [-1],
+    ">= ": [0, 1],
+    "<= ": [0, -1]
 }
+
+
+def version_comparison(a, b):
+    comp_result = apt_pkg.version_compare(a, b)
+    if comp_result > 0:
+        return 1
+    if comp_result < 0:
+        return -1
+    return 0
+
+
+def is_version_in_dependency(dependency, ver):
+    for symbol in SYMBOL_TO_COMPARISON_RESULT_TABLE:
+        if symbol == dependency[0:len(symbol)+1]:
+            dep_vers = dependency.replace(symbol, "")
+            comp_result = version_comparison(dep_vers, ver)
+            if comp_result in SYMBOL_TO_COMPARISON_RESULT_TABLE[symbol]:
+                return True
+            return False
+    raise ValueError("Dependency format not recognised")
 
 
 def get_install_order_for_dependency_list(dep_list):
     pass
 
-# dep_list = {"name", "version"}, final_dep_list = {"name", "version_list"}
-def dependency_list_validation(dep_list):
+
+def populate_dependency_list(dep_list):
     final_dep_list = []
     for dep_to_insert in dep_list:
         for dep_matched in final_dep_list:
@@ -20,10 +44,7 @@ def dependency_list_validation(dep_list):
                 break
 
 
-def concatenate_dependencies(inserted_dep, existing_dep):
-    if ">= " in inserted_dep:
+def get_dependency_list_from_package(pkg):
+    for dep in pkg["pre_depends"]:
 
 
-def apply_filter_on_versions(version_list, filter):
-    for version in version_list:
-        if ">= " == filter[0:3]:
