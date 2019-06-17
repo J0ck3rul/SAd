03d2db4fab0fad8779e2732997bd53fe1499ec24
andreicalinciuc <calinciuc.andrei@yahoo.com>
2019-06-17 19:37:04 +0300

    Create route using apt

--- backend/AptService/aptlib.py
+++ backend/AptService/aptlib.py
@@ -1,13 +1,16 @@
+import json
 import subprocess
 import re
 import tempfile
 from copy import deepcopy
 import time
-from flask import Flask, request, jsonify, send_file
+import flask
 import io
-
 import os
 import sys
+
+from flask import send_file , Flask
+
 sys.path.insert(0, os.path.abspath('..'))
 from PackageClass.package import Package
 
@@ -22,7 +25,8 @@ PKG_CONTENT_MATCHES = {
     r"^Installed-Size: ": "_installed_size",  # in KB
     r"^Size: ": "_download_size",  # in B
     r"^Homepage: ": "_homepage",
-    r"^Maintainer: ": "_maintainer"
+    r"^Maintainer: ": "_maintainer",
+    r"^Architecture": "_architecture"
 
 }
 
@@ -37,12 +41,13 @@ PKG_TEMPLATE = {
     "Depends": [],
     "Conflicts": [],
     "Breaks": [],
-    "Replaces": []
+    "Replaces": [],
+    "Architecture": ""
 }
 
 
 def apt_show(pkg_name):
-    pkg = Package({"name":pkg_name})
+    pkg = Package({"name": pkg_name})
     pkg.__dict__
     cmd_output = subprocess.check_output(['apt-cache', "show", pkg_name]).split("\n")
     for attribute in PKG_CONTENT_MATCHES:
@@ -56,10 +61,52 @@ def apt_show(pkg_name):
 
                 pkg.__dict__[PKG_CONTENT_MATCHES[attribute]] = arguments
 
-    return pkg
+    return pkg.__dict__
 
 
-def apt_search(pkg_name):
+def apt_show_by_version(pkg_name, version):
+    pkg = Package({"name": pkg_name})
+    pkg.__dict__
+    cmd_output = subprocess.check_output(['apt-cache', "show", pkg_name]).split("\n")
+    for attribute in PKG_CONTENT_MATCHES:
+        for line in cmd_output:
+            match = re.search(attribute, line)
+            if match:
+                if isinstance(pkg.__dict__[PKG_CONTENT_MATCHES[attribute]], list):
+                    arguments = re.sub(attribute, "", line).split(", ")
+                else:
+                    arguments = re.sub(attribute, "", line)
+
+                pkg.__dict__[PKG_CONTENT_MATCHES[attribute]] = arguments
+
+    if version in pkg.__dict__["_version"]:
+        return pkg.__dict__
+    else:
+        return []
+
+
+def apt_show_by_version_arch(pkg_name, version, arch):
+    pkg = Package({"name": pkg_name})
+    pkg.__dict__
+    cmd_output = subprocess.check_output(['apt-cache', "show", pkg_name]).split("\n")
+    for attribute in PKG_CONTENT_MATCHES:
+        for line in cmd_output:
+            match = re.search(attribute, line)
+            if match:
+                if isinstance(pkg.__dict__[PKG_CONTENT_MATCHES[attribute]], list):
+                    arguments = re.sub(attribute, "", line).split(", ")
+                else:
+                    arguments = re.sub(attribute, "", line)
+
+                pkg.__dict__[PKG_CONTENT_MATCHES[attribute]] = arguments
+
+    if version in pkg.__dict__["_version"] and arch in pkg.__dict__["_architecture"]:
+        return pkg.__dict__
+    else:
+        return []
+
+
+def apt_search_by_name(pkg_name):
     pkg_list = subprocess.check_output(['apt-cache', "search", pkg_name])
     pkg_name_list = []
     for pkg in pkg_list.split("\n"):
@@ -83,8 +130,5 @@ def apt_download(pkg_name):
         )
 
 
-# print apt_download("python")
-
-
-
-
+if __name__ == "__main__":
+    print apt_download("python")
--- backend/AptService/service.py
+++ backend/AptService/service.py
@@ -1,32 +1,64 @@
-import db
-
-from flask import Flask
+import aptlib
+import json
+from flask import Flask, jsonify
 from flask import request
 from flask import make_response
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
-#get pkg by name
-#find pkg by name
-#get pkg deb by name
-#generate install script
 
 
+# get pkg by name
+# find pkg by name
+# get pkg deb by name
+# generate install script
 
 
-@app.route("/<task_id>", methods = ["GET"])
-def get_pkg_by_name(task_id):
-    return db.getById(task_id)
+@app.route("/search/<pkg_name>", methods=["GET"])
+def find_packages_by_name(pkg_name):
+    try:
+        return jsonify(aptlib.apt_search(pkg_name)), 200
+    except Exception as e:
+        return jsonify({"errormsg": str(e)}), 404
 
-@app.route("/<task_id>", methods = ["GET"])
-def get_task_by_id(task_id):
-    return db.getById(task_id)
 
-@app.route("/<task_id>", methods = ["GET"])
-def get_task_by_id(task_id):
-    return db.getById(task_id)
+@app.route("/package/<pkg_name>", methods=["GET"])
+def get_packages_by_name(pkg_name):
+    try:
+        return jsonify(aptlib.apt_show(pkg_name)), 200
+    except Exception as e:
+        print "Da"
+        return jsonify({"errormsg": str(e)}), 404
+
+
+@app.route("/package/<pkg_name>/<pkg_version>", methods=["GET"])
+def get_packages_by_name_version(pkg_name, pkg_version):
+    try:
+        return jsonify(aptlib.apt_show_by_version(pkg_name, pkg_version)), 200
+    except Exception as e:
+        return jsonify({"errormsg": str(e)}), 404
+
+
+@app.route("/package/<pkg_name>/<pkg_version>/<pkg_architecture>", methods=["GET"])
+def get_package_by_name_version_arch(pkg_name, pkg_version, pkg_architecture):
+    try:
+        return jsonify(aptlib.apt_show_by_version_arch(pkg_name, pkg_version, pkg_architecture)), 200
+    except Exception as e:
+        return jsonify({"errormsg": str(e)}), 404
+
+
+@app.route("/package/<pkg_name>/<pkg_version>/<pkg_architecture>/download", methods=["GET"])
+def download_package_by_name_version_arch(pkg_name, pkg_version, pkg_architecture):
+    try:
+        return send_file(
+            db.download_package_by_name_version_arch(pkg_name, pkg_version, pkg_architecture),
+            "application/vnd.debian.binary-package",
+            as_attachment=True,
+            attachment_filename="{}_{}_{}.deb".format(pkg_name, pkg_version, pkg_architecture)
+        ), 200
+    except Exception as e:
+        return jsonify({"errormsg": str(e)}), 404
 
 
 if __name__ == "__main__":
