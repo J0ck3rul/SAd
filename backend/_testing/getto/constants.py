72b80111e07ad51b8a4b1b8ab53202edff457889
Radu Florin MARINOIU <rmarinoiu@bitdefender.com>
2019-06-21 10:08:08 +0300

    Cleanup older files that are no longer needed

--- backend/_testing/getto/constants.py
+++
@@ -1,4 +0,0 @@
-DB_USERNAME = "devs"
-DB_PASSWORD = "devs"
-
-DISTROS_DB = "distros"
--- backend/_testing/old/main.py
+++
@@ -1,6 +0,0 @@
-# from pmlib.aptlib import apt_show
-from PackageClass import Package
-
-# x=apt_show("python")
-# for key in x.__dict__:
-#     print("{}: {}".format(key,x.__dict__[key]))
